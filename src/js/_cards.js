2bffbe2e15ce28a74d27286f4eda611ab8b0cb24
koz1tsk1y <koz1tsk1y@mail.ru>
2024-08-20 18:58:35 +0300

    Написать стили для секции Помощь и Мышление

--- src/js/_cards.js
+++ src/js/_cards.js
@@ -1,7 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const cards = document.querySelector('.cards');
-  const items = cards.querySelectorAll('.cards__item');
-  const paginationItems = cards.querySelectorAll('.cards__pagination-item');
+const cards = document.querySelectorAll('.cards');
+cards.forEach(card => {
+  const items = card.querySelectorAll('.cards__item');
+  const paginationItems = card.querySelectorAll('.cards__pagination-item');
   let currentIndex = 0;
 
   function updateActiveClasses() {
@@ -14,12 +14,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
   function centerActiveItem() {
     if (currentIndex === 0) {
-      cards.querySelector('.cards__list').style.transform = 'translateX(0)';
+      card.querySelector('.cards__list').style.transform = 'translateX(0)';
     } else {
       const itemWidth = items[currentIndex].offsetWidth;
-      const containerWidth = cards.offsetWidth;
+      const containerWidth = card.offsetWidth;
       const offset = items[currentIndex].offsetLeft - (containerWidth / 2) + (itemWidth / 2);
-      cards.querySelector('.cards__list').style.transform = `translateX(-${offset}px)`;
+      card.querySelector('.cards__list').style.transform = `translateX(-${offset}px)`;
     }
   }
 
@@ -40,15 +40,15 @@ document.addEventListener('DOMContentLoaded', function () {
   let startX = 0;
   let endX = 0;
 
-  cards.addEventListener('touchstart', (e) => {
+  card.addEventListener('touchstart', (e) => {
     startX = e.touches[0].clientX;
   });
 
-  cards.addEventListener('touchmove', (e) => {
+  card.addEventListener('touchmove', (e) => {
     endX = e.touches[0].clientX;
   });
 
-  cards.addEventListener('touchend', () => {
+  card.addEventListener('touchend', () => {
     if (startX > endX + 50) {
       currentIndex = (currentIndex + 1) % items.length;
     } else if (startX < endX - 50) {
